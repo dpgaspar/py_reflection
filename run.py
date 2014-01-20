7453d21840b5cabd831efe9e987ff419193ff59a
dpgaspar <danielvazgaspar@gmail.com>
2014-01-20 22:35:41 +0000

    commit radial

--- run.py
+++ run.py
@@ -4,13 +4,21 @@ from py_reflection import PKGModuleReflection
 
 app = Flask(__name__)
 
-@app.route('/<string:pkg_name>')
-def index(pkg_name='.'):
+@app.route('/normal/<string:pkg_name>')
+def normal(pkg_name='.'):
     cr = PKGModuleReflection(pkg_name)
     #cr.class_tree.debug()
     #cr.class_tree.print_map(map_func=ClassNode.dump)
     data = cr.class_tree.get_json_d3(root_name=pkg_name)
     return render_template('d3tree.html', data = data)
+
+@app.route('/radial/<string:pkg_name>')
+def radial(pkg_name='.'):
+    cr = PKGModuleReflection(pkg_name)
+    #cr.class_tree.debug()
+    #cr.class_tree.print_map(map_func=ClassNode.dump)
+    data = cr.class_tree.get_json_d3(root_name=pkg_name)
+    return render_template('d3treeradial.html', data = data)
 
     
 app.run(host='0.0.0.0', port=8080, debug=True)
