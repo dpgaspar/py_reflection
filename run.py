7cb80a833b9a8d717cb44faeb243f448d40fd2e0
dpgaspar <danielvazgaspar@gmail.com>
2014-01-20 20:51:46 +0000

    corrections

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ from py_reflection import PKGModuleReflection
 
 app = Flask(__name__)
 
-@app.route('/<pkg_name>')
-def index(pkg_name):
+@app.route('/<string:pkg_name>')
+def index(pkg_name='.'):
     cr = PKGModuleReflection(pkg_name)
     #cr.class_tree.debug()
     #cr.class_tree.print_map(map_func=ClassNode.dump)
-    data = cr.class_tree.get_json_d3(root_name='INICIO')
+    data = cr.class_tree.get_json_d3(root_name=pkg_name)
     return render_template('d3tree.html', data = data)
 
     
